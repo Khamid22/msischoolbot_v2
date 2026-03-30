482ca8f58e61cfc880e17364dcb24e6a51af9d52
Khamid22 <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>
2026-03-30 18:48:48 +0500

    Remove obsolete worker process and redundant sheets_data shim

--- app/integrations/sheets/sheets_data.py
+++
@@ -1,8 +0,0 @@
-from . import GroupInfo, SheetsDataError, get_school_dataset, mark_school_dataset_dirty
-
-__all__ = [
-    "get_school_dataset",
-    "mark_school_dataset_dirty",
-    "SheetsDataError",
-    "GroupInfo",
-]
